1df2298aa8e88bcfcc97dab4831ad62181cbb2ba
diegodf8 <diegodf8@gmail.com>
2018-04-13 17:44:49 +0200

    Initial commit

---
+++ src/main/java/com/cice/dbmanager/Manager.java
@@ -0,0 +1,13 @@
+package com.cice.dbmanager;
+
+/**
+ * Calse encargada de generar el acceso y uso de la base de datps
+ */
+
+public class Manager {
+
+    private final String DRIVER ="";
+    
+
+
+}
---
+++ src/test/java/com/cice/dbmanager/AppTest.java
@@ -0,0 +1,20 @@
+package com.cice.dbmanager;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
