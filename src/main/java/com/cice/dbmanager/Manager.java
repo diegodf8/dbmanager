221c5c41a18f13505236b60ea627be0c4c2b3ade
diegodf8 <diegodf8@gmail.com>
2018-04-14 13:25:25 +0200

    New Commit

--- src/main/java/com/cice/dbmanager/Manager.java
+++ src/main/java/com/cice/dbmanager/Manager.java
@@ -1,13 +1,19 @@
 package com.cice.dbmanager;
 
+import javax.sql.RowSet;
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetProvider;
 import java.sql.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Map;
 
 /**
  * Clase encargada de generar el acceso y uso de la base de datps
  */
 
-public class Manager {
+public class Manager{
 
     private final String DRIVER;
     private final String HOST;
@@ -26,7 +32,6 @@ public class Manager {
         this.USER = "root";
         this.PASS = "root";
         this.DATABASE = "diego";
-
     }
 
     public Manager(String DRIVER, String HOST, String PUERTO, String USER, String PASS, String DATABASE) {
@@ -76,19 +81,33 @@ public class Manager {
         return esDesconectado;
     }
 
-    public void ejecutarSelect(String sql){
-        conectarBaseDatos();
+    public CachedRowSet ejecutarSelect(String sql){
+
+        CachedRowSet resultado = null;
+        ResultSet resultSet = null;
+        System.out.println(conectarBaseDatos());
+
         try {
-            ResultSet resultado = statement.executeQuery(sql);
-            while (resultado.next()){
-                int numeroColumnas = resultado.getMetaData().getColumnCount();
-                for (int i = 0 ; i < numeroColumnas ; i++){
-                    String dato = resultado.getString(i);
-                }
-            }
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+            resultado = RowSetProvider.newFactory().createCachedRowSet();
+            resultado.populate(resultSet);
+
+
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                statement.close();
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
         desconectarBaseDatos();
+        return resultado;
     }
+
+
+
 }
